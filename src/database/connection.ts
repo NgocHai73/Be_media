f56a297b627e76cb8ca32c5f8a6ad6576f1dab9e
NgocHai73 <hailngch190599@fpt.edu.vn>
2024-07-15 09:30:23 +0700

    aa

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,10 +3,10 @@ import { createPool, Pool } from 'mysql2/promise';
 export const connect = async (): Promise<Pool> => {
 
     const connection = await createPool({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'social_media',
+        host: 'sql.freedb.tech',
+        user: 'freedb_social_media',
+        password: '6rr6bMbab&r9!qX',
+        database: 'freedb_social_media',
         connectionLimit: 10
     });
 
